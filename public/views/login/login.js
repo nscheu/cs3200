cb297bb088505cb1ef892e06f097742fd9f986b0
nscheu <scheuring.n@husky.neu.edu>
2015-04-19 17:39:09 -0400

    adding my files to start

---
+++ public/views/login/login.js
@@ -0,0 +1,13 @@
+﻿app.controller('LoginCtrl', function ($location, $scope, $http, $rootScope) {
+  //console.log('LoginCtrl Controller');
+  $scope.login = function (user) {
+    console.log(user);
+    $http.post('/login', user)
+    .success(function (response) {
+      console.log(response);
+      $rootScope.currentUser = user;
+      $location.url("/profile");
+    });
+    
+  }
+});
---
+++ public/views/register/register.js
@@ -0,0 +1,15 @@
+﻿app.controller('RegisterCtrl', function ($scope, $http, $rootScope) {
+  
+  $scope.register = function (user) {
+    console.log(user);
+    //include password validation HERE (password and password2)
+    if (user.password == user.password2) {
+      $http.post('/register', user)
+        .success(function (user) {
+          $rootScope.currentUser = user;
+         console.log(user);
+    });
+    }
+    
+  };
+});
